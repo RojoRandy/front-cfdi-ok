edd3bc3c8e976a2257e6012bd5f24d0a0168e2f0
Randy Rojo <rojo.q.randy@gmail.com>
2024-10-22 19:22:02 -0600

    Adjusts profile menu fuctionality

--- src/modules/billing/components/ProfileMenu.vue
+++ src/modules/billing/components/ProfileMenu.vue
@@ -1,26 +1,29 @@
 <template>
-  <div
-    ref="userProfile"
-    @click="toggleMenu = !toggleMenu"
-    class="flex flex-row items-center justify-between bg-slate-800 rounded-full h-full w-52 transition-all duration-200 hover:bg-cyan-600 hover:cursor-pointer"
-  >
-    <span class="text-white flex-1 text-center select-none">Randy Rojo</span>
-    <ProfileIcon class="fill-white w-9 h-9 mr-1" />
-  </div>
-  <div
-    v-if="toggleMenu"
-    ref="floatingElement"
-    :style="floatingStyles"
-    class="flex flex-col gap-2 text-white w-[250px] z-30 bg-slate-800 rounded-lg p-4"
-  >
-    <RouterLink to="profile" class="w-full flex flex-row gap-4 rounded-md py-2 px-2 select-none hover:bg-slate-600 transition-all duration-200 active:bg-cyan-600 hover:cursor-pointer">
-      <ProfileIcon class="fill-white w-6 h-6" />
-      <span>Perfil</span>
-    </RouterLink>
-    <button @click="authStore.logout()"  class="w-full flex flex-row gap-4 rounded-md py-2 px-2 select-none bg-red-500 hover:bg-red-400 transition-all duration-100 active:bg-red-600 hover:cursor-pointer">
-      <LogoutIcon class="fill-white w-6 h-6" />
-      <span>Cerrar Sesión</span>
-    </button>
+  <div ref="profileMenu">
+    <div
+      id="userProfile"
+      ref="userProfile"
+      @click="onToggleClick"
+      class="flex flex-row items-center justify-between bg-slate-800 rounded-full h-full w-52 transition-all duration-200 hover:bg-cyan-600 hover:cursor-pointer"
+    >
+      <span id="userProfileName" class="text-white flex-1 text-center select-none">Randy Rojo</span>
+      <ProfileIcon id="userProfileIcon" class="fill-white w-9 h-9 mr-1" />
+    </div>
+    <div
+      v-if="toggleMenu"
+      ref="floatingElement"
+      :style="floatingStyles"
+      class="flex flex-col gap-2 text-white w-[250px] z-30 bg-slate-800 rounded-lg p-4"
+    >
+      <button @click="goToProfile" class="w-full flex flex-row gap-4 rounded-md py-2 px-2 select-none hover:bg-slate-600 transition-all duration-200 active:bg-cyan-600 hover:cursor-pointer">
+        <ProfileIcon class="fill-white w-6 h-6" />
+        <span>Perfil</span>
+      </button>
+      <button @click="authStore.logout()"  class="w-full flex flex-row gap-4 rounded-md py-2 px-2 select-none bg-red-500 hover:bg-red-400 transition-all duration-100 active:bg-red-600 hover:cursor-pointer">
+        <LogoutIcon class="fill-white w-6 h-6" />
+        <span>Cerrar Sesión</span>
+      </button>
+    </div>
   </div>
 </template>
 
@@ -33,7 +36,7 @@ import ProfileIcon from '@/icons/ProfileIcon.vue';
 import LogoutIcon from '@/icons/LogoutIcon.vue';
 
 import {useAuthStore} from '@/modules/auth/stores/auth.store';
-
+import router from '@/router';
 
 const userProfile = ref(null);
 const floatingElement = ref(null);
@@ -45,9 +48,20 @@ const { floatingStyles } = useFloating(userProfile, floatingElement, {
   whileElementsMounted: autoUpdate,
 });
 
-const authStore = useAuthStore();
+const onToggleClick = () => {
+  toggleMenu.value = !toggleMenu.value;
+}
 
-onClickOutside(floatingElement, () => {
+const profileMenu = ref(null);
+onClickOutside(profileMenu, () => {
   toggleMenu.value = false
 })
+
+const authStore = useAuthStore();
+
+const goToProfile = () => {
+  router.push({name: 'profile'})
+  toggleMenu.value = false
+}
+
 </script>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,10 @@
 import { authRoutes } from '@/modules/auth/routes';
 import { billingRoutes } from '@/modules/billing/routes';
-import ShopLayout from '@/modules/shop/layouts/ShopLayout.vue';
 import { createRouter, createWebHistory } from 'vue-router';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    // {
-    //   path: '/',
-    //   name: 'shop',
-    //   component: ShopLayout,
-    //   children: [
-    //     {
-    //       path: '',
-    //       name: 'Home',
-    //       component: () => import('@/modules/shop/views/HomeView.vue'),
-    //     },
-    //   ],
-    // },
     //Auth Routes
     authRoutes,
 
