8022b84cd4a4315d3a62713791cb70da7b217788
Randy Rojo <rojo.q.randy@gmail.com>
2024-11-13 17:18:47 -0600

    Apply loader in user profile view

--- src/modules/billing/profile/views/UserProfileView.vue
+++ src/modules/billing/profile/views/UserProfileView.vue
@@ -27,17 +27,17 @@ import FiscalAddress from '../components/FiscalAddress.vue';
 import AccountInfo from '../components/AccountInfo.vue';
 import SendMails from '../components/SendMails.vue';
 import DigitalStamp from '../components/DigitalStamp.vue';
-import { onMounted, ref } from 'vue';
+import { onMounted } from 'vue';
+import { useLoadingView } from '@/modules/common/composables/useLoadingView';
 
+const loadingView = useLoadingView()
 import { useRegimenFiscalStore } from '@/modules/catalog/regimen-fiscal/stores/regimen-fiscal.store';
-import LoadingView from '@/modules/common/components/LoadingView.vue';
 import { useMailServerStore } from '@/modules/catalog/mail-server/stores/mail-server.store';
 import { useUserProfileStore } from '../stores/user-profile.store';
 
 const userProfileStore = useUserProfileStore();
 const regimenFiscalStore = useRegimenFiscalStore();
 const mailServerStore = useMailServerStore();
-const isLoading = ref(false);
 
 const sections = [
     {
@@ -68,10 +68,10 @@ const sections = [
   ];
 
 onMounted(async()=> {
-  isLoading.value = true;
+  loadingView.setIsLoading(true);
   await userProfileStore.getUserProfile();
   await regimenFiscalStore.getAllFiscalRegimes();
   await mailServerStore.getMailServers();
-  isLoading.value = false;
+  loadingView.setIsLoading(false);
 })
 </script>
